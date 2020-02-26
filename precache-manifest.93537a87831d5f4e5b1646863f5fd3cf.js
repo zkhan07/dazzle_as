311ca7659f620e256de00b6c3691d4959e28a1c6
zaid <khanzaid786786.kz@gmail.com>
2020-02-26 14:44:02 +0530

    Updates

---
+++ precache-manifest.93537a87831d5f4e5b1646863f5fd3cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78b3b67edbdaec8407b68ce90abbd8b1",
+    "url": "/dazzle_as/index.html"
+  },
+  {
+    "revision": "0a79af435e0f76005d29",
+    "url": "/dazzle_as/static/css/main.a258fd0d.chunk.css"
+  },
+  {
+    "revision": "8944e87e71285e1bf690",
+    "url": "/dazzle_as/static/js/2.48c127f8.chunk.js"
+  },
+  {
+    "revision": "43e75541cb2c99a81f43877710dd8732",
+    "url": "/dazzle_as/static/js/2.48c127f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0a79af435e0f76005d29",
+    "url": "/dazzle_as/static/js/main.bc2e5dfe.chunk.js"
+  },
+  {
+    "revision": "551566814b45e8331c4a",
+    "url": "/dazzle_as/static/js/runtime-main.055efcd3.js"
+  }
+]);
